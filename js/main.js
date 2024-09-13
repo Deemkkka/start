19e54474f45c03a7fc916a3fb396472a360eb2da
Deem <Deemkkka@mail.ru>
2024-09-13 11:38:14 +0300

    2 01-06

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 $(function(){
 
+  $('.carousel__inner').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+  });
+
 });
 
-const fruits = ['apple', 'banana', 'cherry', 'apple', 'banana', 'cherry', 'apple'];
-
-const count = fruits.reduce((accumulator, currentValue) => {
-  accumulator[currentValue] = (accumulator[currentValue] || 0) + 1;
-  return accumulator;
-}, {});
-
-console.log(count);
